b948220d250868c36c318ccd4b81893b6be93f3f
animeshsahu80 <animeshsahu80@gmail.com>
2020-07-10 20:17:05 +0530

    first commit

---
+++ largest_number.cpp
@@ -0,0 +1,12 @@
+static bool mycompare(int& num1, int& num2){
+    return to_string(num1) + to_string(num2) > to_string(num2) + to_string(num1);
+}
+
+string Solution::largestNumber(const vector<int> &arr) {
+    vector<int> nums = arr;
+    sort(nums.begin(),nums.end(),mycompare);
+    string res = "";
+    for(int i = 0; i < (int)nums.size(); i++)
+        res += to_string(nums[i]);
+    return res[0] == '0' ? "0" : res;
+}
