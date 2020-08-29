fe0d920e5aa64cedfd71e291cd0001bfaf0a9189
animeshsahu80 <animeshsahu80@gmail.com>
2020-08-30 02:00:18 +0530

    first commit

---
+++ 0_1_knapsack.cpp
@@ -0,0 +1,13 @@
+int Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    int n=A.size();
+    vector<vector<int>> dp(n+1,vector<int>(C+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=C;j++){
+            if(B[i-1]>j)
+                dp[i][j]=dp[i-1][j];
+            else
+                dp[i][j]=max(dp[i-1][j],A[i-1]+dp[i-1][j-B[i-1]]);
+        }
+    }
+    return dp[n][C];
+}
