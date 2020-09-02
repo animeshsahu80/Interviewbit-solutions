c67463bc84c63d6878c34d8e6055a7bb145f2bd0
animeshsahu80 <animeshsahu80@gmail.com>
2020-09-02 12:31:30 +0530

    first commit

---
+++ min_sum_path_in_mat.cpp
@@ -0,0 +1,16 @@
+int Solution::minPathSum(vector<vector<int> > &A) {
+    int r=A.size();
+    int c=A[0].size();
+    for(int i=1;i<c;i++){
+        A[0][i]+=A[0][i-1];
+    }
+    for(int i=1;i<r;i++){
+        A[i][0]+=A[i-1][0];
+    }
+    for(int i=1;i<r;i++){
+        for(int j=1;j<c;j++){
+            A[i][j]+= min(A[i-1][j],A[i][j-1]);
+        }
+    }
+    return A[r-1][c-1];
+}
