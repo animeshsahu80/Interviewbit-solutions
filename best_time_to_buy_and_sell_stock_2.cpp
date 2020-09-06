c0e72a9c342076eb82f5e6b867fa7786efba0c6a
animeshsahu80 <animeshsahu80@gmail.com>
2020-09-06 12:51:25 +0530

    first commit

---
+++ best_time_to_buy_and_sell_stock_2.cpp
@@ -0,0 +1,10 @@
+int Solution::maxProfit(const vector<int> &A) {
+
+    int profit=0;
+    for(int i=1;i<A.size();i++){
+        if(A[i]<A[i-1])
+            continue;
+        profit+=A[i]-A[i-1];
+    }
+    return profit;
+}
